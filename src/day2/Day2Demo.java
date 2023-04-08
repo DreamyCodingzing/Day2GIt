4b863495616f0cb84a0cfc559d24bb80cf538ee2
Running Bird <Running Bird@DESKTOP-GE19MGH>
2023-04-07 20:30:16 -0400

    push 1

--- src/day2/Day2Demo.java
+++ src/day2/Day2Demo.java
@@ -5,4 +5,8 @@ public class Day2Demo {
 	public static void main(String[] args) {
 		System.out.println("Demo2 Push");
 	}
+	public void getInfo() {
+		System.out.println("pushing code...");
+		
+	}
 }
