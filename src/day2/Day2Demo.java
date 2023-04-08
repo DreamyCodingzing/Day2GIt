7c92b978dee3bf2c6bc18131fae23d7c950bd670
Running Bird <Running Bird@DESKTOP-GE19MGH>
2023-04-07 20:55:52 -0400

    Pushing whatever!

--- src/day2/Day2Demo.java
+++ src/day2/Day2Demo.java
@@ -5,8 +5,8 @@ public class Day2Demo {
 	public static void main(String[] args) {
 		System.out.println("Demo2 Push");
 	}
-	public void getInfo() {
-		System.out.println("pushing code...");
-		System.out.println("Pushing second time");
+	
+	public void whatever() {
+		System.out.println("hello java!");
 	}
 }
